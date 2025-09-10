b92ee51d7e3a14516b8b077147ebd62961134290
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-09-09 21:36:03 -0700

    Change product name from Spring Snap --> Springsnap

--- backend/config/config.py
+++ backend/config/config.py
@@ -28,7 +28,7 @@ RDS_ENGINE = create_engine(
     pool_recycle=3600,
     connect_args={
         "connect_timeout": 10,
-        "application_name": "Firesnaps Backend",
+        "application_name": "Springsnap Backend",
     },
 )
 
