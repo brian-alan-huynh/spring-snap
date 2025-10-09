0dde5af5f8bf69fbca295b39743c2597ffa85eb9
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-10-09 00:59:01 -0700

    Write testing and code analysis script for /backend

--- scripts/run_app_tools.sh
+++ scripts/run_app_tools.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-# runs backend python pytest and pylint scripts to test the app and analyze its code. first script to run the ci/cd pipeline. after that, run the approval step; after approval step, run terraform prod sh
+trap 'echo "Error on line $LINENO"; exit 1' ERR
+trap 'echo "Script finished (exit code $?)"' EXIT
+
+echo "Starting Pylint tests for static code analysis and code quality"
+
+mapfile -t py_files < <(find backend -type f -name "*.py" \
+    ! -path "*/venv/*" \
+    ! -path "*/docker/*" \
+    ! -path "*/tests/*" \
+    ! -path "*/__pycache__/*" \
+    ! -path "*/__init__.py"
+)
+
+if [ "${#py_files[@]}" -gt 0 ]; then
+    pylint "${py_files[@]}"
+else
+    echo "No .py files found for a Pylint analysis"
+fi
+
+echo "Pylint analysis completed"
+
+echo "Starting Pytest tests for unit and integration testing"
+
+pytest --strict-markers --cov=backend --cov-report=term --cov-report=xml:codeCoverageReport.xml
+
+echo "Pytest testing completed"
+
+echo "Testing and analysis completed"
+echo "Pylint code analysis passed"
+echo "Pytests code testing passed"
+echo "Code coverage testing reports generated as an XML file, called codeCoverageReport.xml"
