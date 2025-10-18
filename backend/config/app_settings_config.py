6cd0494f9b605521bffe4c3adb04e01440e0b151
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-10-18 15:25:12 -0700

    Add required type hints

--- backend/config/app_settings_config.py
+++ backend/config/app_settings_config.py
@@ -8,13 +8,13 @@ env = os.getenv
 
 class Settings(BaseSettings):
     environment: str = env("ENVIRONMENT")
-    app_name = "Curby Storage"
-    cors_origins = ["https://curbystorage.com"] if environment == "prod" else ["http://localhost:3000", "https://curbystorage.com"]
-    trusted_hosts = ["api.curbystorage.com"] if environment == "prod" else ["localhost", "127.0.0.1", "api.curbystorage.com"]
+    app_name: str = "Curby Storage"
+    cors_origins: list[str] = ["https://curbystorage.com"] if environment == "prod" else ["http://localhost:3000", "https://curbystorage.com"]
+    trusted_hosts: list[str] = ["api.curbystorage.com"] if environment == "prod" else ["localhost", "127.0.0.1", "api.curbystorage.com"]
 
     csrf_secret_key: str = env("APP_CSRF_SECRET_KEY")
-    csrf_cookie_samesite = "lax"
-    csrf_cookie_secure = True if environment == "prod" else False
-    csrf_token_location = "header"
+    csrf_cookie_samesite: str = "lax"
+    csrf_cookie_secure: bool = True if environment == "prod" else False
+    csrf_token_location: str = "header"
 
-    rate_slowapi_limiter = "50/minute"
+    rate_slowapi_limiter: str = "50/minute"
